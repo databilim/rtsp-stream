c918a22afebbac5f6c6d0b37d28650cd5bf33287
EFAVET <efavet@pc-MacBook-Air-330.local>
2019-05-30 15:35:44 +0300

    camera documnet zoom

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-Stream = require('node-rtsp-stream');
+ Stream = require('node-rtsp-stream');
 stream = new Stream({
     name: 'name',
     streamUrl: 'rtsp://192.168.2.202:554/stream1',
     wsPort: 9999
 });
+ 
